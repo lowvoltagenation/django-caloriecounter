76f9e29dacd01ea9220fe809967a2eaa5fa60395
lowvoltagenation <153026450+lowvoltagenation@users.noreply.github.com>
2024-03-17 12:57:59 -0500

    allowed hosts

--- caloriecounter/settings.py
+++ caloriecounter/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-2hp!eqc3@$aw#-77cx51bqa^^n*h@hczs4m&+$qdzg04gm68w6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
