27680ff1f5aa732dc600bed0794c439c35a376f5
lowvoltagenation <153026450+lowvoltagenation@users.noreply.github.com>
2024-03-17 15:11:53 -0500

    postgres

--- caloriecounter/settings.py
+++ caloriecounter/settings.py
@@ -67,13 +67,23 @@ WSGI_APPLICATION = 'caloriecounter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'caloriecounter',
+        'USER': 'postgres',
+        'PASSWORD': 'Loogie3014@',
+        'HOST': '52.54.98.87',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
